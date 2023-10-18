860d312a228b75e103f66b1713b288ec5016a0dd
Your Global Username <Your Global Username@mail.com>
2023-10-18 14:28:58 +0000

    update strat

--- user_data/strategies/fake_1m_strat.py
+++ user_data/strategies/fake_1m_strat.py
@@ -65,19 +65,55 @@ class fake_1m_strat(IStrategy):
         data_from_feather = feather.read_dataframe('/allah/freqtrade/user_data/strategies/real_1s.feather')
                  
         dataframe['real_1s'] = data_from_feather['real_1s']
-        
-        dataframe.loc[
-                (qtpylib.crossed_above(dataframe['macd'], dataframe['macdsignal'])
-            ),
-            'enter_long'] = 1
+        class DataFrameProcessor:
+            def __init__(self, df):
+                self.df = df
 
+            def prepare_dataframe(self):
+                df_copy = self.df.copy()
+                # df_copy.drop("date", axis=1, inplace=True)
+                df_copy['real_1s'] = pd.to_datetime(df_copy['real_1s'])
+                df_copy = df_copy.sort_values(by='real_1s')
+                return df_copy
 
-        dataframe.loc[
-                (qtpylib.crossed_below(dataframe['macd'], dataframe['macdsignal'])
-            ),
-            'enter_long'] = 1
+            def calculate_aggregated_sum(self, df):
+                return df['volume'].cumsum()
 
-   
+            def aggregate_minute_data(self):
+                aggregated_data = self.df.groupby(self.df['real_1s'].dt.strftime('%Y-%m-%d %H:%M')).apply(self.calculate_aggregated_sum)
+                return aggregated_data
+
+            def calculate_volumes_and_open_sum(self, df):
+                return pd.Series({
+                    'current_minute_volume': df['volume'].sum(),
+                    'open_price': df['open'].iloc[0]
+                })
+
+            def process_data(self):
+                df_test = self.prepare_dataframe()
+                df_test['aggregate_volume_sum'] = df_test.groupby(df_test['real_1s'].dt.strftime('%Y-%m-%d %H:%M')).apply(self.calculate_aggregated_sum).values
+                df_1m_aggregated = df_test.groupby(df_test['real_1s'].dt.strftime('%Y-%m-%d %H:%M')).apply(self.calculate_volumes_and_open_sum)
+                df_1m_aggregated = df_1m_aggregated.reset_index()
+                df_1m_aggregated.columns = ['real_1s', 'current_minute_volume', 'open_price']
+                df_1m_aggregated['last_5_minutes_volumes_sum'] = df_1m_aggregated['current_minute_volume'].rolling(6).sum() - df_1m_aggregated['current_minute_volume']
+                df_test['last_5_minutes_volumes_sum'] = df_test['real_1s'].dt.strftime('%Y-%m-%d %H:%M').map(df_1m_aggregated.set_index('real_1s')['last_5_minutes_volumes_sum'])
+                df_test['open_price'] = df_test['real_1s'].dt.strftime('%Y-%m-%d %H:%M').map(df_1m_aggregated.set_index('real_1s')['open_price'])
+                df_test['entry'] = 0
+                condition = df_test['aggregate_volume_sum'] > df_test['last_5_minutes_volumes_sum']
+                condition = condition & (condition != condition.shift(1))
+                df_test.loc[condition, 'entry'] = 1
+                return df_test
+
+        # Usage
+        df_processor = DataFrameProcessor(dataframe)
+        dataframe = df_processor.process_data()
+            
+        condition_long = (dataframe['entry'] == 1) & (dataframe['close'] > dataframe['open_price'])
+        condition_short = (dataframe['entry'] == 1) & (dataframe['close'] < dataframe['open_price'])
+
+        dataframe.loc[condition_long, 'enter_long'] = 1
+        dataframe.loc[condition_short, 'enter_short'] = 1
+
 
         return dataframe
 
